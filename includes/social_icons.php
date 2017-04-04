32b5007d70aeb17e86406383bb4ea4afbb591d1e
Moises Rabelo de Lima <95274316115@minc.intra>
2017-04-04 11:06:33 -0300

    redes sociais rodape

--- includes/social_icons.php
+++ includes/social_icons.php
@@ -1,37 +1,25 @@
+
 <ul class="et-social-icons">
 
-<?php if ( 'on' === et_get_option( 'divi_show_facebook_icon', 'on' ) ) : ?>
 	<li class="et-social-icon et-social-facebook">
-		<a href="<?php echo esc_url( et_get_option( 'divi_facebook_url', 'https://www.facebook.com/MinisterioDaCultura/?fref=ts' ) ); ?>" class="icon">
-			<span><?php esc_html_e( 'Facebook', 'Divi' ); ?></span>
+		<a href="https://www.facebook.com/MinisterioDaCultura/?fref=ts" class="icon" target="_blank">
+			<span>Facebook</span>
 		</a>
 	</li>
-<?php endif; ?>
-<?php if ( 'on' === et_get_option( 'divi_show_twitter_icon', 'on' ) ) : ?>
 	<li class="et-social-icon et-social-twitter">
-		<a href="<?php echo esc_url( et_get_option( 'divi_twitter_url', 'https://twitter.com/culturagovbr' ) ); ?>" class="icon">
-			<span><?php esc_html_e( 'Twitter', 'Divi' ); ?></span>
+		<a href="https://twitter.com/culturagovbr" class="icon" target="_blank">
+			<span>Twitter</span>
 		</a>
 	</li>
-<?php endif; ?>
-<?php if ( 'on' === et_get_option( 'divi_show_google_icon', 'on' ) ) : ?>
 	<li class="et-social-icon et-social-youtube">
-		<a href="<?php echo esc_url( et_get_option( 'divi_youtube_url', 'https://www.youtube.com/user/ministeriodacultura' ) ); ?>" class="icon">
-			<span><?php esc_html_e( 'youtube', 'Divi' ); ?></span>
+		<a href="https://www.youtube.com/user/ministeriodacultura" class="icon" target="_blank">
+			<span>youtube</span>
 		</a>
 	</li>
-<?php endif; ?>
-<?php if ( 'on' === et_get_option( 'divi_show_rss_icon', 'on' ) ) : ?>
-<?php
-	$et_rss_url = '' !== et_get_option( 'divi_rss_url' )
-		? et_get_option( 'divi_rss_url' )
-		: get_bloginfo( 'rss2_url' );
-?>
 	<li class="et-social-icon et-social-rss">
-		<a href="<?php echo esc_url( $et_rss_url ); ?>" class="icon">
-			<span><?php esc_html_e( 'RSS', 'Divi' ); ?></span>
+		<a href="http://salic.base-wp.cultura.gov.br/feed/" class="icon" target="_blank">
+			<span>RSS</span>
 		</a>
 	</li>
-<?php endif; ?>
 
 </ul>
