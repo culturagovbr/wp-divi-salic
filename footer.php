9fe37dbe4746f08215ea3ef794b5ffbf2037f594
Fernão Lopes <fernao@riseup.net>
2017-11-23 18:53:43 -0200

    removendo entrada manual do piwik

--- footer.php
+++ footer.php
@@ -49,21 +49,5 @@ if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
 
 	<?php wp_footer(); ?>
 
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//analise.cultura.gov.br/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 27]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<noscript><p><img src="//analise.cultura.gov.br/piwik.php?idsite=27" style="border:0;" alt="" /></p></noscript>
-<!-- End Piwik Code -->
-                
 </body>
 </html>
